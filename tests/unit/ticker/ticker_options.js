544e95e062e11007b2919568e53a06be8fa15851
Kai Schlamp <schlamp@gmx.de>
2010-11-09 14:14:57 +0100

    Some little adjustments so tests have enough time.

--- tests/unit/ticker/ticker_options.js
+++ tests/unit/ticker/ticker_options.js
@@ -81,7 +81,7 @@ test("{mouseOffTimeout: 100}", function() {
 		}
 	});
 	
-	window.setTimeout(function() { start(); }, 200 );
+	window.setTimeout(function() { start(); }, 300 );
 });
 
 test("{mouseOnTimeout: 100}", function() {
@@ -107,7 +107,7 @@ test("{mouseOnTimeout: 100}", function() {
 	});
 	
 	$("#ticker").simulate("mouseover");
-	window.setTimeout(function() { start(); }, 200 );
+	window.setTimeout(function() { start(); }, 300 );
 });
 
 test('{nextItem: function() {return $("TestItem")}}', function() {
